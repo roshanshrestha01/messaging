417b5e3cf1556038768a42b96bd6fe432b1fe8a2
wrufesh <wrufesh@gmail.com>
2014-10-20 15:44:59 +0545

    scheduled msg can be editable

--- msgin/tasks.py
+++ msgin/tasks.py
@@ -1,10 +1,6 @@
 from __future__ import absolute_import
-from celery import Celery
-from ms import settings
 from msgin.celery import app
 from msgin.models import Message
-#import time
-#app = Celery('tasks', backend='redis://localhost', broker='redis://localhost:6379/0')
 
 @app.task
 def scheduled_message(m_id):
