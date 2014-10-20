4038a107b2676c7ec1e02c785b799a7600048a50
wrufesh <wrufesh@gmail.com>
2014-10-20 16:54:35 +0545

    scheduled msg editable

--- msgin/tasks.py
+++ msgin/tasks.py
@@ -2,10 +2,9 @@ from __future__ import absolute_import
 from msgin.celery import app
 from msgin.models import Message
 
+
 @app.task
 def scheduled_message(m_id):
-	#time.sleep(sleep_time)
-	message=Message.objects.get(id=m_id)
-	message.status='SEND'
-	message.save()
-    
+    message = Message.objects.get(id=m_id)
+    message.status = 'SEND'
+    message.save()
--- msgin/views.py
+++ msgin/views.py
@@ -3,134 +3,144 @@ from django.http import HttpResponse, HttpResponseRedirect
 from msgin.models import Message, User, Group
 from django.utils import timezone
 from django import forms
-from django.forms import ModelForm
+#from django.forms import ModelForm
 from msgin.tasks import scheduled_message
 from msgin.celery import app
 import re
-import pdb
+#import pdb
+# add one blank line
+
 
 class ComposeMessageForm(forms.Form):
-	user_choice = User.objects.all().values_list('id','username')
-	group_choice = Group.objects.all().values_list('id','name')
-	user_receivers = forms.MultipleChoiceField(user_choice,required=False)
-	group_receivers = forms.MultipleChoiceField(group_choice,required=False)
-	message = forms.CharField(widget=forms.Textarea())
-	schedule = forms.BooleanField(required=False)
-	scheduled_time = forms.DateTimeField(required=False, widget=forms.SplitDateTimeWidget())
+    user_choice = User.objects.all().values_list('id', 'username')
+    group_choice = Group.objects.all().values_list('id', 'name')
+    user_receivers = forms.MultipleChoiceField(user_choice, required=False)
+    group_receivers = forms.MultipleChoiceField(group_choice, required=False)
+    message = forms.CharField(widget=forms.Textarea())
+    schedule = forms.BooleanField(required=False)
+    scheduled_time = forms.DateTimeField(
+        required=False,
+        widget=forms.SplitDateTimeWidget())
+
 
 def get_related_list(obj):
-	l=[]
-	for o in obj:
-		l.append(o.id)
-	return l
+    l = []
+    for o in obj:
+        l.append(o.id)
+    return l
+
 
 def get_waiting_time_sec(time_obj):
-	current_tz = timezone.get_current_timezone()
-	current_time = timezone.now()
-	current_tz_time = current_time.astimezone(current_tz)
-	waiting_time = time_obj - current_tz_time
-	return waiting_time.seconds
+    current_tz = timezone.get_current_timezone()
+    current_time = timezone.now()
+    current_tz_time = current_time.astimezone(current_tz)
+    waiting_time = time_obj - current_tz_time
+    return waiting_time.seconds
+
 
 def get_task_id(ms_id):
-	i=app.control.inspect()
-	sch_ms=i.scheduled()
-	sch_ms_list=sch_ms['celery@localhost.localdomain']
-	#li=[]
-	for l in range(len(sch_ms_list)):
-		smt=sch_ms_list[l]['request']['args']
-		if int(ms_id) == int(re.findall(r'\d*\w',smt)[0]):
-			task_id = sch_ms_list[l]['request']['id']
-	#pdb.set_trace()
-	return task_id
+    i = app.control.inspect()
+    sch_ms = i.scheduled()
+    sch_ms_list = sch_ms['celery@localhost.localdomain']
+    # li=[]
+    for l in range(len(sch_ms_list)):
+        smt = sch_ms_list[l]['request']['args']
+        if int(ms_id) == int(re.findall(r'\d*\w', smt)[0]):
+            task_id = sch_ms_list[l]['request']['id']
+    # pdb.set_trace()
+    return task_id
+
 
 def index(request):
-	return render(request,'msgin/index.html')
-	#return HttpResponse('Here will be compose and inbox')
+    return render(request, 'msgin/index.html')
+    # return HttpResponse('Here will be compose and inbox')
+
 
 def compose(request, msg_id):
-	if msg_id != None:
-		edit=True
-	else:
-		edit=False
-	if request.method == 'POST':
-		form = ComposeMessageForm(request.POST)
-		if form.is_valid():
-			scheduled = False
-			send_by = request.user
-			u_receiver=form.cleaned_data['user_receivers']
-			g_receiver=form.cleaned_data['group_receivers']
-			message_c=form.cleaned_data['message']
-			s_time=form.cleaned_data['scheduled_time']
-			if s_time == None:
-				stat = 'SEND'
-				#Delete here the task while edit
-			else:
-				stat = 'OUTBOX'
-				scheduled=True
-			
-			if edit:
-				app.control.revoke(get_task_id(msg_id), terminate=True)
-				new_message=Message.objects.get(id=msg_id)
-				new_message.group_receiver.clear()
-				new_message.user_receiver.clear()
-			else:
-				new_message=Message()
+    if msg_id is not None:
+        edit = True
+    else:
+        edit = False
+    if request.method == 'POST':
+        form = ComposeMessageForm(request.POST)
+        if form.is_valid():
+            scheduled = False
+            send_by = request.user
+            u_receiver = form.cleaned_data['user_receivers']
+            g_receiver = form.cleaned_data['group_receivers']
+            message_c = form.cleaned_data['message']
+            s_time = form.cleaned_data['scheduled_time']
+            if s_time is None:
+                stat = 'SEND'
+                # Delete here the task while edit
+            else:
+                stat = 'OUTBOX'
+                scheduled = True
 
-			new_message.sender = send_by
-			new_message.message_content = message_c
-			new_message.send_time=s_time
-			new_message.status=stat
-			new_message.save()
-			new_message.group_receiver.add(*g_receiver)
-			new_message.user_receiver.add(*u_receiver)
-			if scheduled:
-				message_id = new_message.id
-				
-				
+            if edit:
+                app.control.revoke(get_task_id(msg_id), terminate=True)
+                new_message = Message.objects.get(id=msg_id)
+                new_message.group_receiver.clear()
+                new_message.user_receiver.clear()
+            else:
+                new_message = Message()
 
+            new_message.sender = send_by
+            new_message.message_content = message_c
+            new_message.send_time = s_time
+            new_message.status = stat
+            new_message.save()
+            new_message.group_receiver.add(*g_receiver)
+            new_message.user_receiver.add(*u_receiver)
+            if scheduled:
+                message_id = new_message.id
 
+                # Inspecting scheduled message
 
-				#Inspecting scheduled message
+                # pdb.set_trace()
+                scheduled_message.apply_async(
+                    (message_id,), countdown=get_waiting_time_sec(
+                        new_message.send_time))
+                # delete the task here
+            return HttpResponseRedirect('/message/')
+    else:
+        if edit:
+            e_message = Message.objects.get(id=msg_id)
+            data = {'user_receivers': get_related_list(e_message.user_receiver.all()),
+                    'group_receivers': get_related_list(e_message.group_receiver.all()),
+                    'message': e_message.message_content,
+                    'scheduled_time': e_message.send_time,
+                    }
+            form = ComposeMessageForm(data)
+        else:
+            form = ComposeMessageForm()
+    context = {'form': form, 'message_id': msg_id}
+    return render(request, 'msgin/compose_message.html', context)
 
-				#pdb.set_trace()
-				scheduled_message.apply_async((message_id,), countdown=get_waiting_time_sec(new_message.send_time))
-				#delete the task here
-			return HttpResponseRedirect('/message/')
-	else:
-		if edit:
-			e_message=Message.objects.get(id=msg_id)
-			data={'user_receivers':get_related_list(e_message.user_receiver.all()),
-				'group_receivers':get_related_list(e_message.group_receiver.all()),
-				'message':e_message.message_content,
-				'scheduled_time':e_message.send_time,
-			}
-			form=ComposeMessageForm(data)
-		else:
-			form=ComposeMessageForm()
-	context = {'form':form, 'message_id':msg_id}
-	return render(request,'msgin/compose_message.html',context)
 
 def inbox(request):
 
+    return HttpResponse('This is inbox bro')
 
-	return HttpResponse('This is inbox bro')
 
 def send(request):
-	pass
+    pass
+
 
 def outbox(request):
-	pass
+    pass
+
 
 def edit_message(request, message_id):
-	if request.method == 'POST':
-		pass
-	else:
-		e_message=Message.objects.get(id=message_id)
-		data={'user_receivers':get_related_list(e_message.user_receiver.all()),
-			'group_receivers':get_related_list(e_message.group_receiver.all()),
-			'message':e_message.message_content,
-			'scheduled_time':e_message.send_time,
-		}
-		form=ComposeMessageForm(data)
-	context = {'form':form, 'message_id':message_id}
-	return render(request,'msgin/compose_message.html',context)
+    if request.method == 'POST':
+        pass
+    else:
+        e_message = Message.objects.get(id=message_id)
+        data = {'user_receivers': get_related_list(e_message.user_receiver.all()),
+                'group_receivers': get_related_list(e_message.group_receiver.all()),
+                'message': e_message.message_content,
+                'scheduled_time': e_message.send_time,
+                }
+        form = ComposeMessageForm(data)
+    context = {'form': form, 'message_id': message_id}
+    return render(request, 'msgin/compose_message.html', context)
